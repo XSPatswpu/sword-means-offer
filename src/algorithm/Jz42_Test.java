0a9599dbe0358f895ed637e394da21a26e557472
xiangshaopeng08028 <xiangshaopeng08028@hellobike.com>
2020-08-07 17:36:54 +0800

    和为S的两个数字

--- src/algorithm/Jz42_Test.java
+++ src/algorithm/Jz42_Test.java
@@ -1,51 +1,62 @@
 package algorithm;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashSet;
 
 /**
  * @author: xiangshaopeng
- * @date: 2020/8/7 15:42
+ * @date: 2020/8/7 16:54
  */
 public class Jz42_Test {
 
     /**
-     * 和为S的连续正数序列
+     * 和为S的两个数字
      * @param args
      */
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         Jz42_Test test = new Jz42_Test();
+        System.out.println(test.FindNumbersWithSum(arr, 7));
 
-        System.out.println(test.FindContinuousSequence(4));
 
     }
 
-    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+    public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
 
-        if (sum <= 1) {
-            return res;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            set.add(array[i]);
         }
 
-        LinkedList<Integer> list = new LinkedList<>();
-        int listSum = 0;
-        int cur = 0;
-        while (cur <= (sum / 2 + 1)) {
-            if (listSum == sum) {
-                res.add(new ArrayList<>(list));
-                cur++;
-                listSum += cur;
-                list.add(cur);
-            } else if (listSum < sum) {
-                cur++;
-                listSum += cur;
-                list.add(cur);
-            } else {
-                Integer head = list.removeFirst();
-                listSum -= head;
+        int minMulti = -1;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            if (set.contains(sum - array[i])) {
+                int multi = array[i] * (sum - array[i]);
+
+                // first
+                if (minMulti < 0) {
+                    minMulti = multi;
+                    result = createList(array[i], sum - array[i]);
+                    continue;
+                }
+
+                if (minMulti > multi) {
+                    minMulti = multi;
+                    result = createList(array[i], sum - array[i]);
+                }
             }
         }
-        return res;
+
+        return result;
     }
 
+    public ArrayList<Integer> createList(int... a) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : a) {
+            list.add(i);
+        }
+        return list;
+    }
 }
